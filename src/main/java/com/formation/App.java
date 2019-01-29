2c6f89dbd370cd5b9b208a9953d234fdaea9e440
kevin gautard <kevin.gautard@gmail.com>
2019-01-29 14:35:26 +0100

    test

--- src/main/java/com/formation/App.java
+++ src/main/java/com/formation/App.java
@@ -5,7 +5,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class App {
 	public static void main(String[] args) {
 		if (args.length < 1) {
-			System.err.println("Please provide an input troubidouche!");
+			System.err.println("Please provide an input troubidouche troubidache!");
 			System.exit(0);
 		}
 		System.out.println(sha256hex(args[0]));
