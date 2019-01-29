129646317e7e565963ed60310189bcee00d756d3
kevin gautard <kevin.gautard@gmail.com>
2019-01-29 14:30:30 +0100

    add troubidouche App.java

--- src/main/java/com/formation/App.java
+++ src/main/java/com/formation/App.java
@@ -5,7 +5,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class App {
 	public static void main(String[] args) {
 		if (args.length < 1) {
-			System.err.println("Please provide an input!");
+			System.err.println("Please provide an input troubidouche!");
 			System.exit(0);
 		}
 		System.out.println(sha256hex(args[0]));
